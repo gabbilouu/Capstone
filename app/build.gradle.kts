2f46fcb3fed9fee576f89f4186fd392ce1b0ed4b
geon-im97 <geon.im97@gmail.com>
2025-04-12 06:30:01 -0500

    not crashing version
    redid the whole project, login button implemented, four bottom buttons not yet implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,18 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     testImplementation(libs.junit)
